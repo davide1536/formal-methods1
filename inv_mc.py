717d251340bdd8ba650919e5e5c58a37e679979d
davide1998 <davide.franzoso@student.unife.it>
2021-11-18 22:40:43 +0100

    qualche commento al programma e un po' di  implementazione dell'algoritmo

--- inv_mc.py
+++ inv_mc.py
@@ -1,6 +1,9 @@
 import pynusmv
 import sys
 
+from pynusmv.prop import not_
+from pynusmv_lower_interface.nusmv.node.node import is_list_empty
+
 def spec_to_bdd(model, spec):
     """
     Return the set of states of `model` satisfying `spec`, as a BDD.
@@ -25,8 +28,37 @@ def check_explain_inv_spec(spec):
     where keys are state and inputs variable of the loaded SMV model, and values
     are their value.
     """
+    fsm = pynusmv.glob.prop_database().master.bddFsm
+    #trasformo la specifica "spec" in bdd
+    bddspec = spec_to_bdd(fsm, spec)
+    
+    #calcolo la negazione del bdd corrispndende a alla specifica "spec"
+    notBddSpec = spec_to_bdd(fsm,pynusmv.prop.not_(spec))
+    print("specifica negata:",notBddSpec )
+
+    #un po' di print per capire 
+    #for state in fsm.pick_all_states(fsm.init):
+    #    print("stati iniziali ", state.get_str_values())
+
+    #for state in fsm.pick_all_states(fsm.post(fsm.init)):
+    #    print("post normale ", state.get_str_values())
+    #
+    #from pynusmv.fsm import BddTrans
+    #trans = BddTrans.from_string(fsm.bddEnc.symbTable,"next(x) = 10")
+    #for state in fsm.pick_all_states(trans.post(fsm.init)):
+        #print("post trans ", state.get_str_values())
+
+    #inizio bfs symbolic
+    reach = fsm.init
+    new = fsm.init
+    
+
+    #queste 2 righe penso che ce le abbia messe il prof visto che la funzione
+    #"check_explain_ltl_spec(ltlspec) fa esattamento quello che richiede l'esercizio"
     ltlspec = pynusmv.prop.g(spec)
     res, trace = pynusmv.mc.check_explain_ltl_spec(ltlspec)
+
+
     return res, trace
 
 if len(sys.argv) != 2:
@@ -38,6 +70,11 @@ filename = sys.argv[1]
 pynusmv.glob.load_from_file(filename)
 pynusmv.glob.compute_model()
 invtype = pynusmv.prop.propTypes['Invariant']
+
+#print(fsm.pick_one_state(fsm.init).get_str_values())
+#for state in fsm.pick_all_states(fsm.post(fsm.init)):
+    #print("state", state.get_str_values())
+#il ciclo serve per controllare ogni proprietà
 for prop in pynusmv.glob.prop_database():
     spec = prop.expr
     if prop.type == invtype:
@@ -50,5 +87,6 @@ for prop in pynusmv.glob.prop_database():
             print(trace)
     else:
         print("Property", spec, "is not an INVARSPEC, skipped.")
-
+   
+   
 pynusmv.init.deinit_nusmv()
